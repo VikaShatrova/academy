81a0e16240733d86f077467f6aef419def6fd007
VikaShatrova <rvrv9696@gmail.com>
2021-01-31 20:55:43 +0300

    hw2

--- academy/src/by/academy/hw/hw2/Task0.java
+++ academy/src/by/academy/hw/hw2/Task0.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Task0 {
-    // Complete the pairs function below.
-
-
     static int pairs(int k, int[] arr) {
         int pairsCount = 0;
-        // write code here
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] - arr[j] == k) {
+                    pairsCount++;
+                }
+            }
+        }
         return pairsCount;
     }
 
@@ -18,20 +21,15 @@ public class Task0 {
     public static void main(String[] args) throws IOException {
 
         String[] nk = scanner.nextLine().split(" ");
-
         int n = Integer.parseInt(nk[0]);
-
         int k = Integer.parseInt(nk[1]);
-
         int[] arr = new int[n];
 
         String[] arrItems = scanner.nextLine().split(" ");
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(arrItems[i]);
-            System.out.println(arr[i]);
         }
-
         int result = pairs(k, arr);
         System.out.println(result);
 
