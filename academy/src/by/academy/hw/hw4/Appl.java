cd174c324a06afa50b82544cc5f30c44875401fa
VikaShatrova <rvrv9696@gmail.com>
2021-02-24 23:43:03 +0300

    !

--- academy/src/by/academy/hw/hw4/Appl.java
+++ academy/src/by/academy/hw/hw4/Appl.java
@@ -1,20 +1,11 @@
 package by.academy.hw.hw4;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Appl {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Date date = new Date();
-
-        System.out.println("input any date");
-        date.setDate(scanner.nextLine());
-        DateValidator dateValidator = new DateValidator();
-
-        if (dateValidator.validate(date.date) == true) {
-            date.showDay(date.date);
-        }
-
 
         scanner.close();
     }
