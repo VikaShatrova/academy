1ae2cb7aafeb2de8e2d68b465529e4b588f6d8b0
Vadim <wadim.sidoryk@mail.ru>
2021-02-24 20:30:24 +0300

    cw10

--- academy/src/by/academy/cw/cw5/IteratorDemo.java
+++ academy/src/by/academy/cw/cw5/IteratorDemo.java
@@ -4,7 +4,7 @@ public class IteratorDemo {
     public static void main(String[] args) {
         Integer[] array = {1, 2, 3, 5, 6, 7, 8, 9};
         Integer[] array1 = {null, null, null};
-        ArrayIterator<Integer> arrayIterator = new ArrayIterator(array);
+        ArrayIterator<Integer> arrayIterator = new ArrayIterator<>(array);
 
         while (arrayIterator.hasNext()) {
             Integer i = arrayIterator.next();
